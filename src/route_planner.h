de1c1b2aa0bc25c9663fce636251636e19774e63
Major Z. Ma <55311663+majorma-cqc@users.noreply.github.com>
2021-04-12 11:51:25 +0800

    Add files via upload

---
+++ src/route_planner.h
@@ -0,0 +1,43 @@
+//
+//  route_planner.h
+//  real_proj1
+//
+//  Created by major ma on 2021/3/29.
+//
+
+#ifndef ROUTE_PLANNER_H
+#define ROUTE_PLANNER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "route_model.h"
+
+
+class RoutePlanner {
+  public:
+    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
+    // Add public variables or methods declarations here.
+    float GetDistance() const {return distance;}
+    // distance is in the private variable
+    void AStarSearch();
+    // statement of A*
+    
+    // The following methods have been made public so we can test them individually.
+    void AddNeighbors(RouteModel::Node *current_node);
+    float CalculateHValue(RouteModel::Node const *node);
+    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+    RouteModel::Node *NextNode();
+
+  private:
+    // Add private variables or methods declarations here. Private variables for openlist.
+    std::vector<RouteModel::Node*> open_list;
+    RouteModel::Node *start_node;
+    RouteModel::Node *end_node;
+
+    float distance = 0.0f;
+    //  distance: complete distance of the path you found.
+    RouteModel &m_Model;
+};
+
+#endif
